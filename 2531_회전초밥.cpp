7ab06e898620c024eb04bd049b04dc4c9cc74f4f
Yh0sun <89528276+Yh0sun@users.noreply.github.com>
2022-03-10 01:06:02 +0900

    백준2531

--- 2531_회전초밥.cpp
+++ 2531_회전초밥.cpp
@@ -1,30 +1,49 @@
 #include<iostream>
-#include<set>
+#include<queue>
 using namespace std;
 
 int main() {
 	int num, various, serial, cupon;
-	int arr[30001];
-	int maxSum = 0;
+	int arr[30001] = { 0, };
+	int sum = 0;
+	int eat[3001] = { 0, };
 
 	cin >> num >> various >> serial >> cupon;
 
 	for (int i = 0; i < num; i++) {
 		cin >> arr[i];
 	}
+	
+	for (int i = 0; i < serial; i++) {
+		eat[arr[i]]++;	
+		if (eat[arr[i]] == 1)sum++;
+	}
 
-	for (int j = 0; j < num; j++) {
-		set<int> eat;
-		int sum;
-		for (int k = j; k < j + serial; k++) {
-			int th = k;
-			if (k >= num) th = k % num;
-			eat.insert(arr[th]);
+	if (eat[cupon] == 0) {
+		eat[cupon] = 1;
+		sum++;
+	}
+	else eat[cupon]++;
+	int maxSum = sum;
+
+	for (int j = 1; j < num  ; j++) {
+		
+		if (eat[arr[j - 1]] == 1) {
+			eat[arr[j - 1]] = 0;
+			sum--;
 		}
-		sum = eat.size();
-		if (eat.find(cupon) == eat.end())sum++;
+		else eat[arr[j - 1]]--;
 
-		if (maxSum < sum)maxSum = sum;
+		int add = j + serial - 1;
+		if (add >= num)add = add % num;
+
+		if (eat[arr[add]] == 0) {
+			eat[arr[add]] = 1;
+			sum++;
+		}
+		else eat[arr[add]]++;
+
+		if (maxSum < sum) maxSum = sum;
 	}
 
 	cout << maxSum;
