fd9e581df482c135c289c709271a38d687a3bc0e
Yh0sun <89528276+Yh0sun@users.noreply.github.com>
2022-03-31 16:09:39 +0900

    프로그래머스-약수의_개수와_덧셈

---
+++ 약수의 개수와 덧셈.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+
+   for (int i = left; i <= right; i++) {
+        double tmp = sqrt(i);
+
+        if (tmp-(int)tmp==0.0)answer -= i;
+        else answer += i;
+    }
+    return answer;
