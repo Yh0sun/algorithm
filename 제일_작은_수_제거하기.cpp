30b2f951479260de4718f299204f1cc5926512a5
Yh0sun <89528276+Yh0sun@users.noreply.github.com>
2022-04-01 18:16:41 +0900

    프로그래머스-제일_작은_수_제거하기

---
+++ 제일_작은_수_제거하기.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer(arr);
+   
+    answer.erase(min_element(answer.begin(), answer.end()));
+    if (answer.empty())answer.push_back(-1);
+
+    return answer;
+}
