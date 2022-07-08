e05a6bb02c654c7737cd1044fdbec6c15dcd8c5c
Yh0sun <89528276+Yh0sun@users.noreply.github.com>
2022-07-08 23:54:54 +0900

    백준16916

--- 16916_부분문자열.cpp
+++ 16916_부분문자열.cpp
@@ -4,45 +4,44 @@ using namespace std;
 
 int main() {
 
-	bool answer = false;
-
 	string str_s, str_p;
 	cin >> str_s >> str_p;
+
+	vector <int> pi(str_p.size());
 	
-	vector <int> pi(str_p.size(), 0);
-	for (int i = 1; i < str_p.size(); i++) {
-		string tmp = str_p.substr(0, i + 1);
-		int cnt = 1;
-		for (int j = i; j > 0; j--) {
-			if (tmp.substr(0, cnt) == tmp.substr(i))pi[i] == cnt;
-			cnt++;
+	
+	for (int i = 1, j = 0; i < str_p.size();) {
+
+		if (str_p[j] == str_p[i]) {
+			pi[i] = ++j;
+			i++;
 		}
-	}
-
-	int j = 0;
-	for (int i = 0; i < str_s.size();) {
-		for (; j < str_p.size(); j++) {
-			if (str_s[i + j] != str_p[j]) {
-				if (j <= 1) {
-					i++;
-					break;
-				}
-				else {
-					if (i + j - pi[j - 1] >= 0)i = i + j - pi[j - 1];
-					else i++;
-
-					j = pi[j - 1];
-
-					break;
-				}
+		else {
+			if (j == 0)i++;
+			while (j > 0 && str_p[j] != str_p[i]) {
+				j = pi[j - 1];
 			}
-
-			if (j == str_p.size() - 1) answer = true;
-			
 		}
-		if (answer)break;
 	}
 
-	cout << answer;
+	int b = 0;
+	for (int a = 0; a < str_s.size();) {
+		if (str_s[a] == str_p[b]) {
+			if (b == str_p.size() - 1) {
+				cout << "1";
+				return 0;
+			}
+			a++;
+			b++;
+		}
+		else {
+			if (b == 0)a++;
+			while (b > 0 && str_s[a] != str_p[b]) {
+				b = pi[b - 1];
+			}
+		}
+	}
+
+	cout << "0";
 	return 0;
 }
