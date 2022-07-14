e29d804806b27e62cbf102936aec29b31eb2c75f
Yh0sun <89528276+Yh0sun@users.noreply.github.com>
2022-07-14 23:53:30 +0900

    시간초과

--- 빛의 경로 사이클.cpp
+++ 빛의 경로 사이클.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 
 using namespace std;
-vector<string> grid;
+vector<vector<int>> history;
 int x_size, y_size;
 
 class mirror {
@@ -27,7 +27,10 @@ public:
         else if (g == 'L') {
             dir += 1;
         }
-        if (dir == 5)dir = 1;
+        else {
+            dir += 2;
+        }
+        if (dir > 4)dir -= 4;
         if (dir == 0)dir = 4;
 
         switch (dir)
@@ -58,7 +61,7 @@ public:
 
 };
 
-vector<int> solution() {
+vector<int> solution(vector<string> grid) {
     vector<int> answer;
     x_size = grid.size();
     y_size = grid[0].size();
@@ -71,15 +74,21 @@ vector<int> solution() {
                 int cnt = 0;
                 tmp.set(k, i, j);
                 vector<int> v = { k,i,j };
-                vector<int> ij = { i,j };
+                vector<vector<int>> ht;
+                ht.push_back({ k,i,j });
                 int x = i;
                 int y = j;
                 do {
                     tmp.move(grid[x][y]);
-                    x=tmp.
+                    x = tmp.x;
+                    y = tmp.y;
                     cnt++;
-                } while (tmp.get() != v);
-                answer.push_back(cnt);
+                    ht.push_back({ tmp.dir,tmp.x,tmp.y });
+                } while (find(history.begin(), history.end(), tmp.get()) == history.end()&&tmp.get() != v);
+               if (tmp.get() == v) {
+                    answer.push_back(cnt);
+                    history.insert(history.end(),ht.begin(),ht.end());
+                }
             }
         }
     }
