37c387428aae1fedb619e2318826c6c83b1e8c97
Yh0sun <89528276+Yh0sun@users.noreply.github.com>
2022-08-05 15:48:02 +0900

    Update 1043_거짓말.cpp

--- 1043_거짓말.cpp
+++ 1043_거짓말.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int find(int a, vector<int>& parent) {
-	if (parent[a] == a) 
+	if (parent[a] == a)
 		return a;
 
 	parent[a] = find(parent[a], parent);
@@ -27,37 +27,32 @@ int main() {
 
 	for (int i = 1; i <= n; i++)
 		parent[i] = i;
-	
+
 	for (int i = 0; i < trueNum; i++)
 		cin >> truePeople[i];
-	
+
 	for (int i = 0; i < m; i++) {
 		int num;
 		cin >> num;
-		
+
 		vector<int> vec(num);
 		for (int j = 0; j < num; j++) {
 			cin >> vec[j];
 			if (j > 0)join(vec[j], vec[j - 1], parent);
 		}
-		
 		attend.push_back(vec);
-	//	cout << attend.size();
-		/*for (int j = 0; j < attend.size(); j++) {
-			for (int k = 0; k < attend[j].size(); k++) {
-				cout << attend[j][k];
-			}
-			cout << endl;
-		}
-		*/
 	}
 
-	for (int i = 0; i < trueNum; i++) {
-		for (int j = 0; j < attend.size(); j++) {
-			if (find(truePeople[i], parent) == find(attend[j][0], parent))lie -= 1;
+	for (int i = 0; i < attend.size(); i++) {
+		for (int j = 0; j < trueNum; j++) {
+			if (find(truePeople[j], parent) == find(attend[i][0], parent)) {
+				lie -= 1;
+				break;
+			}
 		}
 	}
 
 	cout << lie << endl;
+
 	return 0;
 }
