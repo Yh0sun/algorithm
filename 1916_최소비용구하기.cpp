cab5246db233464c8a080c575ec7fec3e3c40f44
Yh0sun <89528276+Yh0sun@users.noreply.github.com>
2022-08-10 03:38:02 +0900

    백준1916

--- 1916_최소비용구하기.cpp
+++ 1916_최소비용구하기.cpp
@@ -1,26 +1,55 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+
 using namespace std;
+struct comp
+{
+	bool operator()(pair<int, int> a, pair<int, int> b) {
+		if (a.second > b.second)
+			return true;
+		else return false;
+	}
+};
 
 int main() {
+
 	int city, bus;
 	cin >> city >> bus;
 
 	vector<vector<pair<int, int>>> table(city + 1, vector < pair<int, int>>());
-	vector<int> dp(city + 1);
+	priority_queue<pair<int, int>, vector<pair<int, int>>, comp> pq;
+	vector<int> min_cost(city + 1,2000000000);
+
 	for (int i = 0; i < bus; i++) {
 		int _start, _end, val;
 		cin >> _start >> _end >> val;
 		table[_start].push_back({ _end,val });
 	}
+
 	int start, end;
 	cin >> start >> end;
 
-	while (true) {
-		for (int i = 0; i < table[start].size(); i++) {
-			pair<int, int> _bus;
-			dp[_bus.first] = _bus.second;
+	min_cost[start] = 0;
+	pq.push({ start,0 });
+
+	while (!pq.empty()) {
+		int next = pq.top().first;
+		int val = pq.top().second;
+		pq.pop();
+
+		if (min_cost[next] < val)continue;
+
+		for (int i = 0; i < table[next].size(); i++) {
+			int ct = table[next][i].first;
+			int cost = table[next][i].second;
+			if (min_cost[ct] > val + cost) {
+				min_cost[ct] = val + cost;
+				pq.push({ ct,min_cost[ct] });
+			}
 		}
 	}
+
+	cout << min_cost[end];
 	return 0;
 }
