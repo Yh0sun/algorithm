277db1c5e2626b326d881306b98166e67e5d619a
Yh0sun <hy770819@naver.com>
2024-01-08 03:47:56 +0900

    add : 백준1202 in java

---
+++ java/src/JewelThief.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class JewelThief {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long ans = 0;
+        int jewelNum = Integer.parseInt(st.nextToken());
+        int bagNum = Integer.parseInt(st.nextToken());
+
+        ArrayList<Jewel> jewels = new ArrayList<>();
+        ArrayList<Integer> bags = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < jewelNum; i++) {
+            st = new StringTokenizer(br.readLine());
+            jewels.add(new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+        for (int i = 0; i < bagNum; i++) {
+            bags.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(jewels);
+        Collections.sort(bags);
+
+        int idxJewel = 0;
+        for (int i = 0; i < bagNum; i++) {
+            int j = idxJewel;
+            for (; j < jewelNum; j++) {
+                if (bags.get(i) < jewels.get(j).getWeight()) break;
+                pq.add(jewels.get(j).getPrice());
+            }
+            idxJewel = j;
+            if (!pq.isEmpty()) ans += pq.poll();
+        }
+        System.out.println(ans);
+    }
+}
+class Jewel implements Comparable<Jewel> {
+    private int weight;
+    private int price;
+
+    Jewel(int w, int p) {
+        this.weight = w;
+        this.price = p;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public int compareTo(Jewel o) {
+        return this.weight - o.weight;
+    }
+}
